eaeb3da15638a3f098e426dc824368531bb27299
TruSerg <truhan_1984@mail.ru>
2021-08-01 19:25:11 +0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+
+$(function () {
+
+	$('.menu-header__icon').click(function (event) {
+		$(this).toggleClass('active');
+		$('.menu-header__menu').toggleClass('active');
+		if ($(this).hasClass('active')) {
+			$('body').data('scroll', $(window).scrollTop());
+		}
+		$('body').toggleClass('lock');
+		if (!$(this).hasClass('active')) {
+			$('body,html').scrollTop(parseInt($('body').data('scroll')));
+		}
+	});
+
+});
+
